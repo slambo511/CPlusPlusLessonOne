a3e1beceef1e561fec38795a05816de90a5f381a
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-05-17 11:48:11 +0100

    getting closer to the end of lesson one.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>                             // Import the basic input/output stream library
-                                                // Whitespace is ignore by the compiler, so use it to structure your code in a readable manner
+#include <iostream>                             // Import the basic input/output stream library  // Import conio, a c header file (Google it!)
+// Whitespace is ignore by the compiler, so use it to structure your code in a readable manner
 using namespace std;                            // Tells the compiler we are using the namespace std, see line 8
 
- int main()                                     // All C++ programs need an entry point, main() is it, int means it returns an integer when it ends, this is used for error handling 0 = OK, 1(other number) = problem
+int main()                                      // All C++ programs need an entry point, main() is it, int means it returns an integer when it ends, this is used for error handling 0 = OK, 1(other number) = problem
 {                                               // Start of main()
     std::cout << "How to print to the console"; // cout outputs text to the console. The angle brackets denote the direction of the things, in this case a string toward cout
-                                                // The std:: point the compiler toward the namespace in which cout resides, here it is the std namespace
-                                                // A namespace is a way of grouping functions and allowing name reuse. So I could create a namespace called sam and a method called cout
-                                                // and reference it using sam::cout. Because we have used the using namespace std; declaration at the start of the code, this is not
-                                                // necessary but is left in here to explain namespaces and using statements in c++
+    // The std:: point the compiler toward the namespace in which cout resides, here it is the std namespace
+    // A namespace is a way of grouping functions and allowing name reuse. So I could create a namespace called sam and a method called cout
+    // and reference it using sam::cout. Because we have used the using namespace std; declaration at the start of the code, this is not
+    // necessary but is left in here to explain namespaces and using statements in c++
     double d;                                   // declaring a variable (a double precision floating point number)
     d = 3.151592654;                            // initialising d with a value, 3.141592654
     int i = 12;                                 // a c++ integer variable holding 12, declared and initialised on the same line
@@ -16,6 +16,12 @@ using namespace std;                            // Tells the compiler we are usi
     cout << endl << "double: " << d;            // the next three lines move to a new line (endl) and output the double, integer and string we just created to the console
     cout << endl << "integer: " << i;
     cout << endl << "string: " << s;
+    cout << endl << "memory address used to store the double: " << &d;      // the next three lines print the addresses where the variable are stored (pointers), as easy as placing an ampersand (&) before a variable name
+    cout << endl << "memory address used to store the integer: " << &i;
+    cout << endl << "memory address used to store the string: " << &s;
 
-    return 0;                                    // main() returns an int and as there where no problems we will return 0
-}                                                // End of main()
+    cout << endl << "press enter to exit" << endl;
+    cin.get();
+
+    return 0x00;                              // main() returns an int and as there where no problems we will return 0 (hex used here)
+}                                             // End of main()
